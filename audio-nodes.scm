45b0da6f129794583a1601ed064e82f0a5068362
Jim Menard <jimm@io.com>
2009-06-19 19:02:46 -0400

    print more audio unit types

--- audio-nodes.scm
+++ audio-nodes.scm
@@ -1,8 +1,18 @@
+(define my-print-au-info
+  (lambda (unit-type-const title)
+     (print title)
+     (au:print-audiounits unit-type-const)))  ;; cals au:get-node-strings
+
 (begin
-  (print "Audio Units")
-  (au:print-audiounits "aumu") ;; cals au:get-node-strings
-  (print "FX Units")
-  (au:print-audiounits "aufx")
+  ; See kAudioUnitType_* constants defined in AUComponent.h
+  (my-print-au-info "auou" "Outputs")
+  (my-print-au-info "aumu" "Audio Units")
+  (my-print-au-info "aufx" "FX Units")
+  (my-print-au-info "aufc" "Format Converters, Spliters, and Mergers")
+  (my-print-au-info "aumx" "Mixers")
+  (my-print-au-info "aupn" "Panners")
+  (my-print-au-info "auol" "Offline Effects")
+  (my-print-au-info "augn" "Generators")
 )
 
 (au:print-graph)
