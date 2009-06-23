4c80044dfe459228392ae190041af6e2d8458623
Jim Menard <jimm@io.com>
2009-06-23 15:12:29 -0400

    Workaround for failure to load all files using map/for-each.

--- flow_loader.scm
+++ flow_loader.scm
@@ -1,4 +1,11 @@
 ;;; Loads flow files in the correct order.
 
-(for-each (lambda (f) (load-my-file (string-append f ".scm")))
-   '("flow_data" "flow_list_gui" "flow"))
+;; For some reason, using load inside map/for-each does not work. Only
+;; the first file gets loaded. I don't know why. Sigh.
+; (for-each
+;   (lambda (f) (print f)  (load-my-file (string-append f ".scm")))
+;   (list "flow_data" "flow_list_gui" "flow"))
+
+(load-my-file "flow_data.scm")
+(load-my-file "flow_list_gui.scm")
+(load-my-file "flow.scm")
