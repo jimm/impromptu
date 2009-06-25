0dcf0e9841e1c16b49d53e1406c599ed914f393d
Jim Menard <jimm@io.com>
2009-06-24 21:50:28 -0400

    removed unneeded test code

--- experiment.scm
+++ experiment.scm
@@ -105,9 +105,6 @@
     ; remaining elements are filters
     ('out *sj* 3)))
 
-
-(list '((kz-pc 0 2)) (list 'out *sj* 3))
-
 (io:print-midi-destinations)
 (stop-midi)
 (set! io:midi-out ())
@@ -137,64 +134,6 @@
 ;;; ================================================================
 ;;; test code
 ;;; ================================================================
-
-;; play one note on sj using raw io:midi-out
-(begin
-  (io:midi-out (now) *sj* *io:midi-on* 3 60 64)
-  (io:midi-out (+ *second* (now)) *sj* *io:midi-off* 3 60 64)
-)
-
-(kz-pc 1 103)
-(kz-pc 1 3)
-
-; play one note on sj using "out". first is note on, second is note off
-(out *sj* 3 *mb* *io:midi-on* 1 60 64)
-(out *sj* 3 *mb* *io:midi-off* 1 60 64)
-
-; This is how to run a function in a constant list. You need to eval the car
-; (the function name).
-(define x '(some-func arg1 arg2 arg3))
-(apply (eval (car x)) (cdr x))
-
-; Example, done explicitly
-(define my-flow
-  (lambda ()
-    (kz-pc 0 3)
-    (set! io:midi-in
-      (lambda (dev type chan a b)
-        (if (= dev *mb*)                ; from mb
-            (if (is-note-type type)
-                (if (and (>= a 60) (<= a 80)) ; range 60 - 80
-                    (io:midi-out (now) *kz* type chan (+ a 12) b))  ; xpose
-                ; non-note passes through
-                (io:midi-out (now) *kz* type chan a b)))))))
-(my-flow)
-
-; Simple mb -> kz
-(set! io:midi-in
-   (lambda (dev typ chan a b)
-      (if (= dev *mb*)
-          (io:midi-out (now) *kz* typ 0 a b))))
-
-(stop-midi)
-
-;;; Graphics noodling
-
-(define *flow-list-canvas* ())
-(define *flow-list-text-style* (gfx:make-text-style "Times-Roman" 16.0 (list 0 0 0 1)))
-
-(define draw-flow-list
-  (lambda (n flow-list)
-     (set! *flow-list-canvas* (gfx:make-canvas 400 600))
-     (gfx:clear-canvas (now) *flow-list-canvas* (list 0 0 0 0.5))
-     (gfx:draw-path (now) *flow-list-canvas*
-                    (gfx:make-rectangle 0 0 400 20)
-                    (list 0 0 0 1)
-                    (list 0 1 0 1))
-     (gfx:draw-text (now) *flow-list-canvas* n *flow-list-text-style* (list 8 0 400 20))))
-
-(draw-flow-list 0 ())
-
 
 (gfx:start-live-video)
 
