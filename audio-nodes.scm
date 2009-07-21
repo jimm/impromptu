685e4712c326e8b8801d0b8e9c519a37db5da28f
Jim Menard <jimm@io.com>
2009-07-21 13:03:40 -0400

    moved some print code from audio-nodes to print-available; fixed some method names

--- audio-nodes.scm
+++ audio-nodes.scm
@@ -1,21 +1,4 @@
-(define my-print-au-info
-  (lambda (unit-type-const title)
-     (print title)
-     (au:print-audiounits unit-type-const)))  ;; cals au:get-node-strings
-
-(begin
-  ; See kAudioUnitType_* constants defined in AUComponent.h
-  (my-print-au-info "auou" "Outputs")
-  (my-print-au-info "aumu" "Audio Units")
-  (my-print-au-info "aufx" "FX Units")
-  (my-print-au-info "aufc" "Format Converters, Spliters, and Mergers")
-  (my-print-au-info "aumx" "Mixers")
-  (my-print-au-info "aupn" "Panners")
-  (my-print-au-info "auol" "Offline Effects")
-  (my-print-au-info "augn" "Generators")
-)
-
-(au:print-graph)
+;;; see also print-available.scm
 
 ; create these dynamically by using au:get-node-strings and picking apart the
 ; returned strings
--- print-available.scm
+++ print-available.scm
@@ -1,14 +1,25 @@
+(define my-print-au-info
+  (lambda (unit-type-const title)
+     (print title)
+     (au:print-audiounits unit-type-const)))  ;; cals au:get-node-strings
+
 (begin
-  (print "Audio Units")
-  (au:print-audiounits "aumu")		; calls au:get-node-strings
-  (print "FX Units")
-  (au:print-audiounits "aufx")
+   ; See kAudioUnitType_* constants defined in AUComponent.h
+   (my-print-au-info "auou" "Outputs")
+   (my-print-au-info "aumu" "Audio Units")
+   (my-print-au-info "aufx" "FX Units")
+   (my-print-au-info "aufc" "Format Converters, Spliters, and Mergers")
+   (my-print-au-info "aumx" "Mixers")
+   (my-print-au-info "aupn" "Panners")
+   (my-print-au-info "auol" "Offline Effects")
+   (my-print-au-info "augn" "Generators")
 )
+
 
 (au:print-graph)
 
-(io:midi-print-sources)
-(io:midi-print-destinations)
+(io:print-midi-sources)
+(io:print-midi-destinations)
 
 (define (split-into-lines str)
   nil)
