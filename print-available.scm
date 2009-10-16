ce0030f489a88f0a8f5935e71319f797695925dd
Jim Menard <jimm@io.com>
2009-10-16 18:37:54 -0400

    improved node detection code

--- print-available.scm
+++ print-available.scm
@@ -1,37 +1,23 @@
+;;; See also audio-nodes.scm
+
 (define my-print-au-info
   (lambda (unit-type-const title)
-     (print title)
-     (au:print-audiounits unit-type-const)))  ;; cals au:get-node-strings
+    (print title)
+    (au:print-audiounits unit-type-const)))  ;; cals au:get-node-strings
 
 (begin
-   ; See kAudioUnitType_* constants defined in AUComponent.h
-   (my-print-au-info "auou" "Outputs")
-   (my-print-au-info "aumu" "Audio Units")
-   (my-print-au-info "aufx" "FX Units")
-   (my-print-au-info "aufc" "Format Converters, Spliters, and Mergers")
-   (my-print-au-info "aumx" "Mixers")
-   (my-print-au-info "aupn" "Panners")
-   (my-print-au-info "auol" "Offline Effects")
-   (my-print-au-info "augn" "Generators")
+  ; See kAudioUnitType_* constants defined in AUComponent.h
+  (my-print-au-info "auou" "Outputs")
+  (my-print-au-info "aumu" "Audio Units")
+  (my-print-au-info "aufx" "FX Units")
+  (my-print-au-info "aufc" "Format Converters, Spliters, and Mergers")
+  (my-print-au-info "aumx" "Mixers")
+  (my-print-au-info "aupn" "Panners")
+  (my-print-au-info "auol" "Offline Effects")
+  (my-print-au-info "augn" "Generators")
 )
-
 
 (au:print-graph)
 
 (io:print-midi-sources)
 (io:print-midi-destinations)
-
-(define (split-into-lines str)
-  nil)
-
-(define (parse-params node-str)
-  nil)
-
-(define (replace-node-name node-list)
-  nil)
-
-(define (node-list node-type)
-  ; create list of node lists
-  (let ((str (au:get-node-strings node-type)))
-    (map (lambda (node-str) (replace-node-name (parse-params node-str)))
-	 (split-into-lines str))))
