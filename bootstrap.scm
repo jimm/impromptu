4480a5d03bc5155a1e0abe86119457673f2c32ea
Jim Menard <jim.menard@kaplan.com>
2011-06-17 16:14:30 -0400

    comment

--- bootstrap.scm
+++ bootstrap.scm
@@ -6,9 +6,10 @@
 ;;; NOTE: this file is loaded twice by Impromptu: once when loading the
 ;;; primary process and once when loading the utility process.
 
-;; The root directory of this repository on my system. I can't figure out
-;; how to get any environment variables, or I'd use HOME instead of this
-;; hardcoded value.
+;; The root directory of this repository on my system. I've figured out how
+;; to read environment variables, but very few are defined inside Imprompt.
+;; For example, HOME is defined byt none of the ones defined in my .bashrc
+;; are defined. Thus the hard-coded value.
 (define *my-dir* "/Users/jimm/src/impromptu/")
 
 (define load-my-file (lambda (f) (load (string-append *my-dir* f ".scm"))))
