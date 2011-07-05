d4f7d35f548aff19511b8a8a8eb4b496d9a189a8
Jim Menard <jim@jimmenard.com>
2011-07-05 13:51:06 -0400

    new require function

--- init.scm
+++ init.scm
@@ -3,3 +3,16 @@
 
 (print "Hello, World!")
 (macro (comment args) ())               ; (comment (ignored stuff))
+
+(define required-files ())
+
+(define require
+  (lambda args
+    (for-each
+     (lambda (arg)
+       (if (member arg required-files)
+           #t
+           (begin
+             (load-my-file arg)
+             (set! required-files (cons arg required-files)))))
+     args)))
