aa176d09564a43563a807ec9ac100cdf4a3be737
Jim Menard <jim@jimmenard.com>
2011-07-06 20:52:30 -0400

    Var name change. Comment.

--- bootstrap.scm
+++ bootstrap.scm
@@ -6,12 +6,25 @@
 ;;; NOTE: this file is loaded twice by Impromptu: once when loading the
 ;;; primary process and once when loading the utility process.
 
-;; The root directory of this repository on my system. I've figured out how
-;; to read environment variables, but very few are defined inside Imprompt.
-;; For example, HOME is defined byt none of the ones defined in my .bashrc
-;; are defined. Thus the hard-coded value.
+;; *my-dir* is the root directory of this repository on my system. It should
+;; end with a slash.
+;;
+;; Instead of hard-coding this path here I'd rather read this value from an
+;; environment. Unfortunately very few env vars are defined inside
+;; Impromptu. For example, HOME is defined but none of the ones defined in
+;; my .bashrc are defined. Thus the hard-coded value.
+;;
+;; By the way, here's how to read the environment variable HOME:
+;;
+;; (let* ((proc-info (objc:call "NSProcessInfo" "processInfo"))
+;;        (env-dict (objc:call proc-info "environment"))
+;;        (env-var (objc:call env-dict "objectForKey:" "HOME")))
+;;    (objc:nsstring->string env-var))
+
 (define *my-dir* "/Users/jimm/src/impromptu/")
 
+;; This method loads a file in *my-dir*. The ".scm" extension gets added
+;; automatically.
 (define load-my-file (lambda (f) (load (string-append *my-dir* f ".scm"))))
 
 (load-my-file "init")
--- init.scm
+++ init.scm
@@ -1,18 +1,20 @@
 ;;; This file is loaded by bootstrap.scm when Impromptu loads.
 ;;; See the comment there.
 
-(print "Hello, World!")
 (macro (comment args) ())               ; (comment (ignored stuff))
 
-(define required-files ())
+;; Holds the list of previously required files. See require below.
+(define *required-files* ())
 
+;; Loads each argument as a Lisp file using load-my-file. Each file is
+;; loaded only if it has not been loaded before.
 (define require
   (lambda args
     (for-each
      (lambda (arg)
-       (if (member arg required-files)
+       (if (member arg *required-files*)
            #t
            (begin
              (load-my-file arg)
-             (set! required-files (cons arg required-files)))))
+             (set! *required-files* (cons arg *required-files*)))))
      args)))
